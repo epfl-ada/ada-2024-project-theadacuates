7611e629b2ea94a4f96898ce26d6882aabb3b61c
8gabri8 <gabri.dalai@gmail.com>
2024-12-03 16:51:39 +0100

    finish entropy

--- static/assets/js/global.js
+++ static/assets/js/global.js
@@ -1,6 +1,7 @@
 $(function () {
   $("#part1").load("./components/part1.html");
   $("#intro").load("./components/0_intro.html");
+  $("#readability").load("./components/3_readability.html");
   $("#entropy").load("./components/4_entropy.html");
   // $("#part2").load("./components/part2.html");
 });

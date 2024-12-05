6759ee89a49253d24705fbd0a4a0d66fd1856b75
Danielacoco <danielacoor6@gmail.com>
2024-12-05 23:35:47 +0100

    some changes to intro and conectivity part to website

--- static/assets/js/global.js
+++ static/assets/js/global.js
@@ -3,5 +3,7 @@ $(function () {
   $("#intro").load("./components/0_intro.html");
   $("#readability").load("./components/3_readability.html");
   $("#entropy").load("./components/4_entropy.html");
+  $("#connectivity").load("./components/5_connectivity.html");
+
   // $("#part2").load("./components/part2.html");
 });

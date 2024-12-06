b751d4d23d9d7ee47b1c32045e09cfe5b5dd15b8
Danielacoco <danielacoor6@gmail.com>
2024-12-06 14:15:22 +0100

    main page clean up

--- static/assets/js/global.js
+++ static/assets/js/global.js
@@ -1,5 +1,5 @@
 $(function () {
-  $("#part1").load("./components/part1.html");
+  $("#cover").load("./components/00_cover.html");
   $("#intro").load("./components/0_intro.html");
   $("#pathlength").load("./components/2_time_length.html");
   $("#readability").load("./components/3_readability.html");
@@ -7,6 +7,4 @@ $(function () {
   $("#cognitive_shift").load("./components/6_cognitive_shift.html");
   $("#connectivity").load("./components/5_connectivity.html");
   $("#position").load("./components/7_position.html");
-
-  // $("#part2").load("./components/part2.html");
 });
